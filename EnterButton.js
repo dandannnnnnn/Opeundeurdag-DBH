86c081dae02359c41c42e92ea0e504a4d01f1fc0
WoutVVD <184236120+WoutVVD@users.noreply.github.com>
2025-02-04 15:05:17 +0100

    Update EnterButton.js

--- EnterButton.js
+++ EnterButton.js
@@ -1,7 +1,8 @@
-/* Selecteer de knop met het id 'mijnKnop'
-const knop = document.getElementById('enterButton'); */
+/* Selecteer de knop met het id 'mijnKnop'*/
+const knop = document.getElementById('enterButton'); 
 
-/* Voeg een event listener toe voor de klik
+/* Voeg een event listener toe voor de klik */
 knop.addEventListener('click', function() {
     window.location.href = '../quiz/vraag1.html';
-}); */
+});
+alt f4
